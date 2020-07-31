c82c8d15ba26744cbd1a0373bc0549e880f94ef6
svogeti <sameervogeti@gmail.com>
2020-07-31 22:19:41 +0530

    SumRangeRecursion

---
+++ src/SumRangeRecursion.java
@@ -0,0 +1,21 @@
+/*
+* Program to Add the numbers in a given range recursively.
+* Example :- sumRange(5) should return 5+4+3+2+1=15
+* */
+
+public class SumRangeRecursion {
+    int sumRange(int range)
+    {
+        if(range==1)
+        {
+            return 1;
+        }
+        return range+sumRange(range-1);
+    }
+    public static void main(String args[])
+    {
+        SumRangeRecursion sumRangeRecursion=new SumRangeRecursion();
+        System.out.println(sumRangeRecursion.sumRange(2));
+
+    }
+}
